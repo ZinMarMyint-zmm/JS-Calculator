d8a3f5cb6afdcd8fa97767cabe7a2593226b6ffd
stellazmm <zmar9251@gmail.com>
2022-05-06 19:36:09 +0630

    update

--- js/app.js
+++ js/app.js
@@ -1,12 +1,46 @@
 let displayBox = document.querySelector(".display");
 let keypad = document.querySelector(".keypad");
+let operators = ["+","-","*","/","%"];
 
-let showInDisplay = (i)=> displayBox.innerText +=i;
+let filter = x =>{
+    let current = displayBox.innerText;
+    let lastChar = current[current.length-1];
+
+    if(current.length > 10 ){
+        return false;
+    }
+
+    if(current== "0" && x != "."){
+        clearLast();
+    }
+    if(operators.includes(x) && operators.includes(lastChar)){
+        return false;
+    }
+    return true;
+}
+
+let showInDisplay = (x)=> {
+    if(filter(x)){
+        displayBox.innerText +=x;
+    }
+}
     
-let calc=()=> displayBox.innerText = eval(displayBox.innerText);
-
+let calc=()=> {
+    if(!operators.includes(displayBox.innerText[displayBox.innerText.length-1])){
+        displayBox.innerText = eval(displayBox.innerText);
+    }
+    
+}
 let clearAll = _ => displayBox.innerText="";
 
 let clearLast = _ => displayBox.innerText=displayBox.innerText.substring(0,displayBox.innerText.length-1);
+
+keypad.addEventListener("click",_=>{
+    if(displayBox.innerText.length>10){
+        displayBox.style.fontSize = 30+"px";
+    }else if(displayBox.innerText.length>5){
+        displayBox.style.fontSize = 60+"px";
+    }
+})
              
         
